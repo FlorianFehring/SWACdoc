e4778d06ea5c7d1a6642d3aeadaf5ab62c843f73
FlorianFehring <florian.fehring@hsbi.de>
2025-01-27 13:54:35 +0100

    Added: Tryout for removeing lazy loaded data and load new

--- js/visualisation/present.js
+++ js/visualisation/present.js
@@ -29,6 +29,14 @@ document.addEventListener('swac_present_example7_complete', function (evt) {
         let comp = document.querySelector('#present_example7').swac_comp;
         comp.addDataLazy();
     });
+    
+    let present_example7_rem = document.querySelector('.present_example7_rem');
+    present_example7_rem.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        let comp = document.querySelector('#present_example7').swac_comp;
+        comp.removeAllData();
+        comp.addDataFromReference('ref://exampledata_biglist.json');
+    });
 });
 
 var present_example8_options = {
