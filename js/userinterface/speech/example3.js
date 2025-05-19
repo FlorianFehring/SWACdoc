c6e55ac38cfa3974fce6bf0f8ac17d5405200235
FlorianFehring <florian.fehring@hsbi.de>
2025-05-19 15:31:34 +0200

    Changed: Speech: Example 3 reduced to minimal configuration

--- js/userinterface/speech/example3.js
+++ js/userinterface/speech/example3.js
@@ -1,22 +1,4 @@
 var speech_example3_options = {
     lang: 'de-DE',
-	startword: 'Computer',
-	    commands: {
-        'de-DE': {
-            'rede': {
-                speak: 'Hallo'
-            },
-            'zeige': {
-                execute: function() { window.alert('Hallo!');}
-            }
-        },
-        'en-EN': {
-            'say': {
-                speak: 'Hello'
-            },
-            'show': {
-                execute: function() { window.alert('Hello!');}
-            }
-        }
-    }
+    startword: 'Computer'
 };
