a34449d0cba2c4289adeae687ac22b998aad1890
Maczap99 <90683916+Maczap99@users.noreply.github.com>
2025-12-08 14:38:30 +0100

    Changed: seperate example 1-14 from present

---
+++ js/visualisation/present/example12.js
@@ -0,0 +1,11 @@
+window['present_example12_options'] = {
+    plugins: new Map()
+};
+window['present_example12_options'].plugins.set('FilterSort', {
+    id: 'FilterSort',
+    active: true
+});
+
+window['FilterSort_present_example12_options'] = {
+    filterable: false
+};
