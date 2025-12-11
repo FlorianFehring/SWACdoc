6c3652f90ea5fe4fcaf22580970abedbcf966716
FlorianFehring <ffehring@hsbi.de>
2025-12-11 06:30:48 +0100

    Changed: Resturcurted booking example

---
+++ js/input/booking/example1.js
@@ -0,0 +1,8 @@
+window['booking_example1_options'] = {
+    bookingsRequestor: {
+        fromName: '../../../data/input/booking/example1/rooms_bookings.json'
+    },
+    bookRequestor: {
+        fromName: '../../../data/input/booking/example1/create.json'
+    }
+};
---
+++ js/input/booking/example2.js
@@ -0,0 +1,8 @@
+window['booking_example2_options'] = {
+    bookingsRequestor: {
+        fromName: '../../../data/input/booking/example/events_bookings.json'
+    },
+    bookRequestor: {
+        fromName: '../../../data/input/booking/example1/create.json'
+    }
+};
