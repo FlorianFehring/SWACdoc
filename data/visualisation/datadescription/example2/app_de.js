da1cd42e01614be86d96e4fb14cee18068d528f3
FlorianFehring <florian.fehring@hsbi.de>
2025-12-17 12:47:03 +0100

    Changed: Datadescription: Fixed some parts in example2.

---
+++ data/visualisation/datadescription/example2/app_de.js
@@ -0,0 +1,12 @@
+var app_de = {
+    "doubleval_title": "Double-Wert",
+    "doubleval_desc": "Ein Double-Wert",
+    "doubleval_value_low": "Ein niedriger Wert",
+    "doubleval_value_mid": "Ein mittlerer Wert",
+    "doubleval_value_high": "Ein hoher Wert",
+    "strings_title": "String",
+    "strings_desc": "Ein String-Wert",
+    "strings_value_first": "Erster String Wert",
+    "strings_value_second": "Zweiter String Wert"
+};
+export default app_de;
