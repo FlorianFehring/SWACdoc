57241bc1b06d4b891613564557ded7570597b1e8
Maczap99 <90683916+Maczap99@users.noreply.github.com>
2025-12-18 11:04:10 +0100

    Change (Labeling): change file structure, file path and change json

---
+++ js/input/labeling/example2.js
@@ -0,0 +1,9 @@
+window['labeling_example1_options'] = {
+    showWhenNoData: true,
+    labelidAttr: 'label_id',
+    labeledidAttr: 'oo_id',
+    labelSource: {
+        fromName: '../../../data/input/labeling/label_labels.json'
+    },
+    showNoDataInfo: false
+};
--- js/input/labeling/index.js
+++ js/input/labeling/index.js
@@ -1,13 +1,3 @@
 window['explaincomponent_options'] = {
     componentName: 'Labeling'
 };
-
-window['labeling_example1_options'] = {
-    showWhenNoData: true,
-    labelidAttr: 'label_id',
-    labeledidAttr: 'oo_id',
-    labelSource: {
-        fromName: '../../../data/input/labeling/label_labels.json'
-    },
-    showNoDataInfo: false
-};
