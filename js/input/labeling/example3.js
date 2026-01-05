47a96acc70ce92ce5575872d6de6246b8332c881
Maczap99 <90683916+Maczap99@users.noreply.github.com>
2026-01-05 15:04:28 +0100

    Change (Labeling): add example 3 for css classen and add coder in all html files

---
+++ js/input/labeling/example3.js
@@ -0,0 +1,9 @@
+window['labeling_example3_options'] = {
+    showWhenNoData: true,
+    labelidAttr: 'label_id',
+    labeledidAttr: 'oo_id',
+    labelSource: {
+        fromName: '../../../data/input/labeling/example3/label_example3.json'
+    },
+    showNoDataInfo: false
+};
