07052d150495a6131bd9db7272aa65d0c4b3e047
FlorianFehring <florian.fehring@hsbi.de>
2026-01-15 09:30:44 +0100

    Changed: Overworked media/player documentation

---
+++ js/media/player/example11.js
@@ -0,0 +1,7 @@
+window['player_example11_options'] = {
+    allowDel: true,
+    customAfterRemoveSet: function (requestor) {
+        // Code to directly execute after playlist entry was removed
+        // e.g. remove connection from database
+    }
+};
---
+++ js/media/player/example9.js
@@ -0,0 +1,5 @@
+window['player_example9_options'] = {
+    commentRequestor: {
+        fromName: '../../../data/media/player/example9/comments.json'
+    }
+};
